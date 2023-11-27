fd19db4261b0e09a1950c0a3f6511c2819a7b077
Abhiram Kantipudi <78171758+Abhiram716@users.noreply.github.com>
2023-11-27 14:12:01 +0000

    Sync LeetCode submission - Single Number III (javascript)

---
+++ problems/problems/single_number_iii/solution.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var singleNumber = function (nums) {
+    let singleElements = [];
+    if (nums.length === 1) {
+        return nums[0];
+    }
+
+    let map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+
+        if (map.has(nums[i])) {
+            map.set(nums[i], map.get(nums[i]) + 1)
+        }
+
+        else {
+            map.set(nums[i], 1);
+        }
+    }
+
+    for (let [key, value] of map) {
+        if (map.get(key) === 1) singleElements.push(key);
+    }
+
+    return singleElements;
+};

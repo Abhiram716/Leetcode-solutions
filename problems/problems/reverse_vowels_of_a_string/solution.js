da559a4ef02a828c5dc1a8adb7e2850d48d7af74
Abhiram Kantipudi <78171758+Abhiram716@users.noreply.github.com>
2023-12-07 20:10:04 +0000

    Sync LeetCode submission - Reverse Vowels of a String (javascript)

---
+++ problems/problems/reverse_vowels_of_a_string/solution.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
+var reverseVowels = function(s) {
+   const vowels = 'aeiouAEIOU';
+  let left = 0;
+  let right = s.length - 1;
+  const sArray = s.split(''); // Convert the string to an array for easier modification
+
+  while (left < right) {
+    // Move the left pointer to the next vowel
+    while (left < right && !vowels.includes(sArray[left])) {
+      left++;
+    }
+
+    // Move the right pointer to the next vowel
+    while (left < right && !vowels.includes(sArray[right])) {
+      right--;
+    }
+
+    // Swap the vowels at the left and right pointers
+    [sArray[left], sArray[right]] = [sArray[right], sArray[left]];
+
+    left++;
+    right--;
+  }
+
+  // Convert the array back to a string
+  return sArray.join('');
+};
